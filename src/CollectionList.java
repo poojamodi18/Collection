0bf66b7536d9585ef4503d4ee2c31165dbb51423
poojamodi18 <pooja18modi@gmail.com>
2022-01-17 14:46:06 +0530

    commit

--- src/CollectionList.java
+++ src/CollectionList.java
@@ -3,24 +3,24 @@ import java.util.Collections;
 
 public class CollectionList {
     public static void main(String[] args) {
-        ArrayList<String> list=new ArrayList<String>();//Creating arraylist
-        list.add("Mango");//Adding object in arraylist
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Mango");
         list.add("Apple");
         list.add("Banana");
         list.add("Grapes");
 
-         list.add(1,"One");
-      //Printing the arraylist object
-      System.out.println(list);
-      //for-each
-      for(String s: list){
-         System.out.println(s);
-      }
-      System.out.println(list.get(0));
-      list.remove(1);
-      System.out.println(list);
+        list.add(1, "One");
+        //Printing the arraylist object
+        System.out.println(list);
+        //for-each loop
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println(list.get(0));
+        list.remove(1);
+        System.out.println(list);
 
-      Collections.sort(list);
-      System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
     }
 }
