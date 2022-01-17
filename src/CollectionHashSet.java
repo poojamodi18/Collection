d290b98633ef3fcbc54ea444b4ff67421a8b5be9
poojamodi18 <pooja18modi@gmail.com>
2022-01-17 15:21:09 +0530

    commit

--- src/CollectionHashSet.java
+++ src/CollectionHashSet.java
@@ -1,3 +1,5 @@
+import pojo.Book;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,5 +11,22 @@ public class CollectionHashSet {
         set1.add("one");
         System.out.println(set1);
 
+        //Hashset with class object as value
+        Book book1 = new Book(101, "Operating System", "Galvin", "Wiley");
+        Book book2 = new Book(102, "DCN", "Forouzan", "Mc Hill");
+
+        Set<Book> bookSet = new HashSet<Book>();
+        bookSet.add(book1);
+        bookSet.add(book2);
+
+        System.out.println(bookSet);
+
+        for (Book book : bookSet) {
+
+//            System.out.println(book);
+            System.out.println(book.getName() + " " + book.getId());
+
+
+        }
     }
 }
