adb5c6c97a94b17c833e3312fba0287b3043da6f
poojamodi18 <pooja18modi@gmail.com>
2022-01-19 11:55:34 +0530

    commit

--- src/BookData.java
+++ src/BookData.java
@@ -5,27 +5,63 @@ import java.util.ArrayList;
 public class BookData {
     private ArrayList<Book> bookList = new ArrayList<Book>();
 
-    BookData(){
-        Book book1 = new Book(101, "Operating System", "Galvin", "Wiley",150);
-        Book book2 = new Book(102, "DCN", "Forouzan", "Mc Hill",450);
-        Book book3 = new Book(103,"Data Communication","Wiley","Md Hill",350);
+    BookData() {
+        Book book1 = new Book(101, "Operating System", "Galvin", "Wiley", 150);
+        Book book2 = new Book(102, "DCN", "Forouzan", "Mc Hill", 450);
+        Book book3 = new Book(103, "Data Communication", "Wiley", "Md Hill", 350);
         bookList.add(book1);
         bookList.add(book2);
         bookList.add(book3);
     }
 
-    public void addNewBookDetail(Book book){
+    public void addNewBookDetail(Book book) {
         bookList.add(book);
     }
 
-    public void showBookDetails(){
-        for (Book b: bookList) {
+    public int newBookId() {
+        Book b = bookList.get(bookList.size() - 1);
+        return b.getId() + 1;
+    }
+
+    public void showAllBookDetails() {
+        for (Book b : bookList) {
             System.out.println(b.toString());
         }
     }
-    public boolean removeBookDetail(int id){
-        for (Book b: bookList) {
-            if(b.getId()==id){
+
+    public void showBookDetailsByID(int id) {
+        for (Book b : bookList) {
+            if (b.getId() == id) {
+                System.out.println(b.toString());
+                return;
+            }
+        }
+        System.out.println("Book with ID " + id + " does not exist");
+    }
+
+    public void showBookDetailsByName(String bookName) {
+        for (Book b : bookList) {
+            if (b.getName().equalsIgnoreCase(bookName)) {
+                System.out.println(b.toString());
+                return;
+            }
+        }
+        System.out.println("Book with name " + bookName + " does not exist");
+    }
+
+    public boolean removeBookDetailByID(int id) {
+        for (Book b : bookList) {
+            if (b.getId() == id) {
+                bookList.remove(b);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean removeBookDetailByName(String bookName) {
+        for (Book b : bookList) {
+            if (b.getName().equalsIgnoreCase(bookName)) {
                 bookList.remove(b);
                 return true;
             }
--- src/BookDemo.java
+++ src/BookDemo.java
@@ -1,10 +1,100 @@
+import model.Book;
+
+import java.util.Scanner;
+
 public class BookDemo {
     public static void main(String[] args) {
         BookData bookData = new BookData();
-        bookData.showBookDetails();
-        if(bookData.removeBookDetail(102)){
-            System.out.println("Book Detail Removed Successfully");
-            bookData.showBookDetails();
-        }
+
+        boolean exit = true;
+        do {
+            System.out.println("1 - Show All Book Details");
+            System.out.println("2 - Show One Book details");
+            System.out.println("3 - Add New Book");
+            System.out.println("4 - Remove Book");
+            System.out.println("5 - Exit");
+            System.out.print("Choice: ");
+            Scanner sc = new Scanner(System.in).useDelimiter("\n");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    bookData.showAllBookDetails();
+                    break;
+                case 2:
+                    System.out.println("1 - Show book details by id");
+                    System.out.println("2 - Show book details by name");
+                    System.out.println("3 - Main Menu");
+                    System.out.print("Choice: ");
+                    int showChoice = sc.nextInt();
+                    switch (showChoice) {
+                        case 1:
+                            System.out.print("Enter Book ID: ");
+                            int bookID = sc.nextInt();
+                            bookData.showBookDetailsByID(bookID);
+                            break;
+                        case 2:
+                            System.out.print("Enter Book Name: ");
+                            String bookName = sc.next();
+                            bookData.showBookDetailsByName(bookName);
+                            break;
+                        case 3:
+                            continue;
+                        default:
+                            System.out.println("Invalid Choice");
+                            break;
+                    }
+                    break;
+                case 3:
+                    Book newBook = new Book();
+                    System.out.print("Enter Book Name:");
+                    newBook.setName(sc.next());
+                    System.out.print("Book Author:");
+                    newBook.setAuthor(sc.next());
+                    System.out.print("Book Publisher:");
+                    newBook.setPublisher(sc.next());
+                    System.out.print("Book Price:");
+                    newBook.setPrice(sc.nextInt());
+                    newBook.setId(bookData.newBookId());
+                    bookData.addNewBookDetail(newBook);
+                    break;
+                case 4:
+                    System.out.println("1 - Remove book by id");
+                    System.out.println("2 - Remove book by name");
+                    System.out.println("3 - Main Menu");
+                    System.out.print("Choice: ");
+                    int removeChoice = sc.nextInt();
+                    switch (removeChoice) {
+                        case 1:
+                            System.out.print("Enter Book ID: ");
+                            int bookID = sc.nextInt();
+                            if (bookData.removeBookDetailByID(bookID)) {
+                                System.out.println("Book with ID " + bookID + " removed");
+                            } else {
+                                System.out.println("Book with ID " + bookID + " does not exist");
+                            }
+                            break;
+                        case 2:
+                            System.out.print("Enter Book Name: ");
+                            String bookName = sc.next();
+                            if(bookData.removeBookDetailByName(bookName)){
+                                System.out.println("Book with name " + bookName + " removed");
+                            }else {
+                                System.out.println("Book with name " + bookName + " does not exist");
+                            }
+                            break;
+                        case 3:
+                            continue;
+                        default:
+                            System.out.println("Invalid Choice");
+                            break;
+                    }
+                    break;
+                case 5:
+                    exit = false;
+                    break;
+                default:
+                    System.out.println("Invalid Choice");
+            }
+        } while (exit);
     }
 }
