4968fca45d53f539f25eae5820386909fb88f5bc
poojamodi18 <pooja18modi@gmail.com>
2022-01-19 17:27:23 +0530

    commit

--- src/CmdLine.java
+++ src/CmdLine.java
@@ -1,5 +1,6 @@
 public class CmdLine {
     public static void main(String[] args) {
+        //run in cmd
         for (int i=0;i<args.length;i++){
             System.out.println(args[i]);
         }
