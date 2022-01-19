f95bf8161b891ff95083bca709f5f8a51db2ab8b
poojamodi18 <pooja18modi@gmail.com>
2022-01-19 11:02:48 +0530

    commit

--- src/BookData.java
+++ src/BookData.java
@@ -8,8 +8,10 @@ public class BookData {
     BookData(){
         Book book1 = new Book(101, "Operating System", "Galvin", "Wiley",150);
         Book book2 = new Book(102, "DCN", "Forouzan", "Mc Hill",450);
+        Book book3 = new Book(103,"Data Communication","Wiley","Md Hill",350);
         bookList.add(book1);
         bookList.add(book2);
+        bookList.add(book3);
     }
 
     public void addNewBookDetail(Book book){
@@ -20,5 +22,14 @@ public class BookData {
         for (Book b: bookList) {
             System.out.println(b.toString());
         }
+    }
+    public boolean removeBookDetail(int id){
+        for (Book b: bookList) {
+            if(b.getId()==id){
+                bookList.remove(b);
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/BookDemo.java
+++ src/BookDemo.java
@@ -2,6 +2,9 @@ public class BookDemo {
     public static void main(String[] args) {
         BookData bookData = new BookData();
         bookData.showBookDetails();
-
+        if(bookData.removeBookDetail(102)){
+            System.out.println("Book Detail Removed Successfully");
+            bookData.showBookDetails();
+        }
     }
 }
