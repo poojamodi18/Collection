5d584a04a453e0d8e3857669c2fad3109f5d1d3c
poojamodi18 <pooja18modi@gmail.com>
2022-01-19 18:20:59 +0530

    commit

--- src/serviceinterface/BookDataInterface.java
+++ src/serviceinterface/BookDataInterface.java
@@ -3,11 +3,11 @@ package serviceinterface;
 import model.Book;
 
 public interface BookDataInterface {
-    public void addNewBookDetail(Book book);
-    public int newBookId();
-    public void showAllBookDetails();
-    public void showBookDetailsByID(int id);
-    public void showBookDetailsByName(String bookName);
-    public boolean removeBookDetailByID(int id);
-    public boolean removeBookDetailByName(String bookName);
+    void addNewBookDetail(Book book);
+    int newBookId();
+    void showAllBookDetails();
+    void showBookDetailsByID(int id);
+    void showBookDetailsByName(String bookName);
+    boolean removeBookDetailByID(int id);
+    boolean removeBookDetailByName(String bookName);
 }
