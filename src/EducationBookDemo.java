c58ad9b258119930153edf78d4877d709a7da896
poojamodi18 <pooja18modi@gmail.com>
2022-01-20 10:42:51 +0530

    Commit

--- src/EducationBookDemo.java
+++ src/EducationBookDemo.java
@@ -2,8 +2,10 @@ import model.EducationalBook;
 
 public class EducationBookDemo {
     public static void main(String[] args) {
-        EducationalBook educationalBook = new EducationalBook(101,"Complete Java Reference","Herbert",
-                "McGraw Hill",540,"Textbook","Java");
+        EducationalBook educationalBook = new EducationalBook(101, "Complete Java Reference", "Herbert",
+                "McGraw Hill", 540, "Textbook", "Java");
         System.out.println(educationalBook.toString());
+        //access super class method by using subclass's object
+        System.out.println("Book Name: "+educationalBook.getName());
     }
 }
